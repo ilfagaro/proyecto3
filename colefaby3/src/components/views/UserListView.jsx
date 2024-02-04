85881ff2ff74cb7833fbca2b38ea8acbd2cb91d7
ilfagaro <ildafabianagaonarotela@gmail.com>
2024-02-04 22:30:57 +0100

    subir configuracion

--- colefaby3/src/components/views/UserListView.jsx
+++ colefaby3/src/components/views/UserListView.jsx
@@ -1,10 +1,14 @@
-import UserList from "../components/UserList";
+// UserListView.jsx
+import React from 'react';
+import UserList from '../components/UserList';
 
-function UserListView(){
-    return(
-        
-        <UserList/>
-    )
+function UserListView() {
+  return (
+    <div>
+      <h1>Arrabal Scool</h1>
+      <UserList />
+    </div>
+  );
 }
 
 export default UserListView;
--- colefaby3/src/main.jsx
+++ colefaby3/src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import UserListView from './views/UserListView'
+// main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import UserListView from '../views/UserListView';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
-    <UserListView/>
-    
+    <UserListView />
   </React.StrictMode>,
-)
+);
+
